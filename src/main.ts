f444c4a3523cd30499a8da1da21a7d6ceb58f9d8
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-12-10 14:10:02 +0100

    📝 docs(README.md): add detailed project information and setup instructions 🔧 refactor(main.ts): enhance Swagger UI setup and add bearer auth to API documentation The README.md file has been updated to provide a comprehensive overview of the project, including its features, prerequisites, and setup instructions. This will help new contributors understand the project better and set it up more easily. In main.ts, the Swagger UI setup has been enhanced with custom CSS and JS for better UI/UX. Bearer authentication has been added to the API documentation to provide information about the authentication method used in the API.

--- src/main.ts
+++ src/main.ts
@@ -29,13 +29,28 @@ async function bootstrap() {
   });
 
   const config = new DocumentBuilder()
-    .setTitle('Example API')
+    .setTitle('Backend API')
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('example')
+    .setBasePath('api/v1')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
+
+  // Restrict CORS to your frontend app domain
+  // if (process.env.NODE_ENV !== 'production') {
+  //   app.enableCors();
+  // }
 
   await app.listen(process.env.PORT || 3000);
 }
