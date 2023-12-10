b86272397a852866db8684054e32a017a1bd2d7b
Milan_sacahni <milan.sachani.ms@gmail.com>
2023-12-10 10:41:23 +0100

    🔧 refactor(main.ts): enable CORS to restrict requests to frontend app domain ✨ feat(main.ts): add support for process.env.PORT environment variable to run app on a configurable port 📦 new file(vercel.json): add configuration for Vercel deployment
    The CORS policy has been updated to restrict requests to the frontend app domain, enhancing the security of the application. The application can now run on any available port specified via the process.env.PORT environment variable, increasing its flexibility. A new file, vercel.json, has been added to configure the deployment of the application on Vercel, enabling continuous deployment and integration.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Restrict CORS to your frontend app domain
+  // const configService = app.get(ConfigService);
+  // const frontendUrl = configService.get('FRONTEND_URL');
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    allowedHeaders:
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    maxAge: 3600,
+    exposedHeaders: 'Authorization',
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
